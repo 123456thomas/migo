c0d99582b26ae39032c020b218e508bfcb78c878
123456thomas <17625809083@sina.cn>
2018-12-30 18:28:43 +0800

    提交增加

--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
-class 
+class UserProfile(models.Model):
+    """定义了一个用户扩展数据类型，要和系统内置的用户一对一关联"""
+    id = models.AutoField(primary_key=True)
+    header = models.ImageField(upload_to='static/images/headers/',
+                               default='static/images/headers/default.png')
+    nickname = models.CharField(max_length=50)
+    phone = models.CharField(max_length=20)
+    age = models.InterrField(default=0)
+    gender = models.CharField(max_length=5)
+
+    # 和系统内置的用户关联
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+# 注册
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'users/register.html',{})
+    elif request.method == 'POST':
+        # 获取用户数据
+        username = request.POST['username']
+        password = request.POST['password']
+        nickname = request.POST['nickname']
+
+        # 判断数据是否可用
+        try:
+            user = User.objects.get(username=username)
+            return render(request, 'users/register.html',
+                          {'error_code': -1, 'error_msg': '该账号已经存在，请使用其它账号注册'})
+        except:
+            # 判断昵称是否可用
+            try:
+                user_profile = User.userprofile.objects.get(nicjname=nickname)
+                return render(request,'users/register.html',
+                            {'error_code':-2,'error_msg':'该昵称已经存在，请使用其它昵称'})
+            except:
+                user = User.objects.create_user(username=username, password=password)
